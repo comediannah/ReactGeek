9ab61e06f25e560c9e686b87a04ae474d2fb8146
comediannah <istominjob@gmail.com>
2021-11-18 12:32:01 +0300

    l5

--- src/components/ChatList/index.js
+++ src/components/ChatList/index.js
@@ -1,33 +1,48 @@
 import React from "react";
 import { v4 as uuid } from 'uuid';
 import { Link, Navigate } from 'react-router-dom';
+import { initChats } from "../../utils/constants";
 import './ChatList.css';
 
 const ChatList = ({ chatID }) => {
-    const [chats] = React.useState([{
-        name: "Глупый бот",
-        id: "FoolBot"
-      }, {
-        name: "Злой бот",
-        id: "AngryBot"
-      }, {
-        name: "Бот-зануда",
-        id: "BoringBot"
-      }]);
+  const [chats, setChats] = React.useState(initChats);
+  const [newChatName, setNewChatName] = React.useState('');
+
+  const addChat = event => {
+    event.preventDefault();
+    setChats( [ ...chats, {name: newChatName, id: uuid()} ] );
+    setNewChatName('');
+  }
 
   if (!( chatID && chats.find(chat => chat.id === chatID))) {
     return (<Navigate replace to="/chats/FoolBot" />);
   }
 
   return (
-    <ul className="chatList">
-    {
-      chats.map(chat => <li
-        key={ uuid() }
-        className={chat.id === chatID ? "activeChat" : "passiveChat"}
-      ><Link to={chat.id}>{chat.name}</Link></li>)
-    }
-    </ul>
+    <div className="chatListContainer">
+      <ul className="chatList">
+      {
+        chats.map(chat => <li
+          key={ uuid() }
+          className={chat.id === chatID ? "activeChat" : "passiveChat"}
+        >
+          <button className="deleteBtn" onClick={
+            () => {
+              setChats(chats.filter(el => el.id !== chat.id))
+            }
+          }>–</button>
+          <Link to={chat.id}>{chat.name}</Link></li>)
+      }
+      </ul>
+      <form onSubmit={addChat}>
+        <input
+          type="text"
+          value={newChatName}
+          onChange={event=>setNewChatName(event.target.value)}
+        />
+        <input type="submit" value="+"/>
+      </form>
+    </div>
     );
 }
 
